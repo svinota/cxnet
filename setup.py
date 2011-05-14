0535f1f1af1f617386d0043c5b7fbfad423ed883
peet <peet@4ee366cd-6e07-0410-ac6f-9ccd36be5cc6>
2011-05-14 12:46:47 +0000

    test change for git-svn
    git-svn-id: svn://radlinux.org/radlinux/branches/0.7.0@1775 4ee366cd-6e07-0410-ac6f-9ccd36be5cc6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
 	name="cxnet",
-	version="0.7.0",
+	version="0.7.1",
 	url="http://www.radlinux.org/connexion/",
 	author="Peter V. Saveliev",
 	author_email="peet@altlinux.org",
