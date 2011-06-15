24eb319ad27fee056765438816e528a4d2f8efc0
Peter V. Saveliev <peet@altlinux.org>
2011-06-15 15:15:38 +0400

    ident fixed

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(
-	name="cxnet",
-	version="0.7.1",
-	url="http://projects.radlinux.org/cx/",
-	author="Peter V. Saveliev",
-	author_email="peet@altlinux.org",
-	license="GPLv3",
-	packages=[
-		"cxnet",
-		"cxnet.netlink",
-	],
+    name="cxnet",
+    version="0.7.1",
+    url="http://projects.radlinux.org/cx/",
+    author="Peter V. Saveliev",
+    author_email="peet@altlinux.org",
+    license="GPLv3",
+    packages=[
+        "cxnet",
+        "cxnet.netlink",
+    ],
 )
