ec0615b3e689976af90f3911ea6658d80468fa34
Peter V. Saveliev <peet@altlinux.ru>
2011-08-12 17:46:48 +0400

    update module index file

--- cxnet/netlink/__init__.py
+++ cxnet/netlink/__init__.py
@@ -17,8 +17,8 @@
 #     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 __all__ = [
+    "core",
     "generic",
-    "genetlink",
     "ipq",
     "rtnl",
     "iproute2",
